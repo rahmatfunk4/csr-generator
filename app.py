c21f082544f2fdaa9b9e18e32045b3b7671a48f2
Takamasa Sakai <sakai@serverworks.co.jp>
2019-03-03 13:36:16 +0900

    鍵とCSRをダウンロードできるようにした

--- app.py
+++ app.py
@@ -4,12 +4,16 @@ from cryptography.hazmat.primitives.asymmetric import rsa
 from cryptography.hazmat.primitives import hashes
 from cryptography import x509
 from cryptography.x509.oid import NameOID
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, send_file
+import io
 
 app = Flask(__name__)
 
 
 def gen_rsa_key():
+    """
+    秘密鍵を2048bitで生成
+    """
     return rsa.generate_private_key(
         public_exponent=65537,
         key_size=2048,
@@ -18,6 +22,9 @@ def gen_rsa_key():
 
 
 def gen_csr(key, c, s, l, o, ou, cn):
+    """
+    CSRをSHR256で生成
+    """
     return x509.CertificateSigningRequestBuilder().subject_name(x509.Name([
         x509.NameAttribute(NameOID.COUNTRY_NAME, c),
         x509.NameAttribute(NameOID.STATE_OR_PROVINCE_NAME, s),
@@ -30,11 +37,17 @@ def gen_csr(key, c, s, l, o, ou, cn):
 
 @app.route('/')
 def form():
+    """
+    トップページ表示
+    """
     return render_template('form.html')
 
 
 @app.route('/confirm', methods=['POST', 'GET'])
 def confirm():
+    """
+    POSTで受け取ったデータを元にCSRを生成して結果を表示
+    """
     if request.method == 'POST':
         country = request.form["Country"]
         state = request.form["State"]
@@ -62,7 +75,38 @@ def confirm():
 
         csr_pem = csr.public_bytes(serialization.Encoding.PEM)
 
-        return render_template("confirm.html", privete_key_pem=privete_key_pem.decode('utf-8'), csr_pem=csr_pem.decode('utf-8'), result=result)
+        return render_template(
+            "confirm.html",
+            privete_key_pem=privete_key_pem,
+            csr_pem=csr_pem,
+            result=result)
+
+
+@app.route('/download_private_key', methods=['POST'])
+def download_private_key():
+    """
+    生成された秘密鍵をダウンロードさせる
+    """
+    if request.method == 'POST':
+        private_key = io.BytesIO(request.form['private-key'].encode())
+
+        return send_file(
+            private_key,
+            attachment_filename="test.pem",
+            as_attachment=True)
+
+
+@app.route('/download_csr', methods=['POST'])
+def download_csr():
+    """
+    生成されたCSRをダウンロードさせる
+    """
+    if request.method == 'POST':
+        csr = io.BytesIO(request.form['csr'].encode())
+        return send_file(
+            csr,
+            attachment_filename="test.csr",
+            as_attachment=True)
 
 
 if __name__ == "__main__":
