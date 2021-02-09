83c477dfc3d63353fa5c7b14b351312517d01357
rahmatfunk4 <rahmat.funk4@gmail.com>
2021-02-09 10:31:50 +0700

    Update app.py

--- app/src/app.py
+++ app/src/app.py
@@ -92,7 +92,7 @@ def download_private_key():
 
         return send_file(
             private_key,
-            attachment_filename=request.form['CommonName'] + "_key.pem",
+            attachment_filename=request.form['CommonName'] + "_key.key",
             as_attachment=True)
 
 
@@ -105,7 +105,7 @@ def download_csr():
         csr = io.BytesIO(request.form['csr'].encode())
         return send_file(
             csr,
-            attachment_filename=request.form['CommonName'] + "_csr.pem",
+            attachment_filename=request.form['CommonName'] + "_csr.txt",
             as_attachment=True)
 
 
